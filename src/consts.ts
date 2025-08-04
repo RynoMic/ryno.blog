0fe1decf187691edbeac4c66ce4c9b6b85a8c567
ryan anderson <ryanmanderson1986@gmail.com>
2025-08-03 19:14:07 -0500

    initial commit. Astro Blog template.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'W.I.T.S.';
+export const SITE_DESCRIPTION = 'A blog asking the real questions?';
