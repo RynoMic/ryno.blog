3f42909d8ba41302373f1852da4a3e93cb0588cb
RynoMic <ryan.michael.prod@gmail.com>
2025-12-01 20:16:32 -0600

    remove main image

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
 import __ASTRO_IMAGE_IMPORT_ZJf45J from "../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md";
-import __ASTRO_IMAGE_IMPORT_OCiBr from "../../assets/blog-placeholder-2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdev%2Fthird-post.md";
-export default new Map([["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_ZJf45J], ["../../assets/blog-placeholder-2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdev%2Fthird-post.md", __ASTRO_IMAGE_IMPORT_OCiBr]]);
+export default new Map([["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_ZJf45J]]);
 		
