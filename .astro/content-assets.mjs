318d73be11eaaa1d01e22843d1be46eb90648a13
RynoMic <ryan.michael.prod@gmail.com>
2025-12-23 18:40:39 -0600

    update theming

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
 import __ASTRO_IMAGE_IMPORT_ZJf45J from "../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md";
-import __ASTRO_IMAGE_IMPORT_ZXikxU from "../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftraining.md";
-export default new Map([["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_ZJf45J], ["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftraining.md", __ASTRO_IMAGE_IMPORT_ZXikxU]]);
+export default new Map([["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_ZJf45J]]);
 		
