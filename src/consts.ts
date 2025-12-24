59e3fc37fd014440865237d6335431bd6810e4b6
RynoMic <ryan.michael.prod@gmail.com>
2025-12-23 19:13:37 -0600

    remove question mark animation

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'W.I.T.S.';
-export const SITE_DESCRIPTION = 'A blog asking the real questions?';
+export const SITE_DESCRIPTION = 'Ain\'t much of a legacy';
