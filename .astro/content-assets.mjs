89798140ffdd6d23b51465a31d71d48fa1171e05
Ryan Michael Anderson <ryanmichael72@gmail.com>
2026-04-01 07:20:53 -0500

    add vault-cms

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_ZJf45J from "../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md";
-export default new Map([["../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_ZJf45J]]);
+import __ASTRO_IMAGE_IMPORT_a6cKb from "../../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fryan.michael.content%2Fposts%2Fnumber1.md";
+export default new Map([["../../../../public/numberonepaint.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fryan.michael.content%2Fposts%2Fnumber1.md", __ASTRO_IMAGE_IMPORT_a6cKb]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,14 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    assetsInclude: ['**/*.base', '**/.obsidian/**', '**/_bases/**'],
+    server: {
+      watch: {
+        ignored: ['**/.obsidian/**', '**/_bases/**', '**/bases/**', '**/_home/**', '**/home/**', '**/_base/**', '**/base/**']
+      }
+    }
+  },
 	site: 'http://localhost:4321',
 	integrations: [mdx(), sitemap()],
 	output: 'server',
